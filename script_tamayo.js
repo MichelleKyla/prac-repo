855adccd51ea86d2717b4a73a8b043ddabb560af
Michelle Kyla Tamayo <michelletamayo931@gmail.com>
2024-10-15 21:40:05 +0800

    Added Tamayo's JS File

---
+++ script_tamayo.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+    const sections = document.querySelectorAll('.interests .card-container');
+    sections.forEach(section => section.style.display = 'none');
+    const kdramaSection = document.getElementById('aspirations');
+    kdramaSection.style.display = 'block';
+
+    document.querySelectorAll('.navbar a').forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            sections.forEach(section => section.style.display = 'none');
+            document.getElementById(link.getAttribute('href').substring(1)).style.display = 'block';
+        });
+    });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dateElement = document.getElementById('current-date');
+    const currentDate = new Date().toLocaleDateString();
+    dateElement.textContent = `${currentDate}`;
+});
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', function() {
+        const modalId = this.getAttribute('data-target');
+        document.getElementById(modalId).style.display = 'block';
+    });
+});
+
+document.querySelectorAll('.close').forEach(closeBtn => {
+    closeBtn.addEventListener('click', function() {
+        this.closest('.modal').style.display = 'none';
+    });
+});
+
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = 'none';
+    }
+};
